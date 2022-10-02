295dad5f3f74bb9cf3843ad21536a0ce4490ef56
Sasha <a.lange@rambler.ru>
2022-10-02 12:59:09 +0300

    Я сделал

--- Programm 1/Program.cs
+++ Programm 1/Program.cs
@@ -1,4 +1,4 @@
-﻿int oper;
+﻿int a;
 do
 {
     Console.WriteLine("Выберите какую операцию вы хотите сделать: ");
@@ -13,56 +13,61 @@ do
                       "9. Выйти из программы\r\n");
 
 
-    oper = Convert.ToInt32(Console.ReadLine());
-    switch (oper)
+    a = Convert.ToInt32(Console.ReadLine());
+    switch (a)
     {
         case 1:
-            Console.WriteLine("Напишите два числа");
-            int ab1 = Convert.ToInt32(Console.ReadLine());
-            int ab2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(ab1 + ab2);
+            Console.WriteLine("Введите два числа, которые хотите сложить");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(b + c);
             break;
         case 2:
-            Console.WriteLine("Напишите два числа");
-            int ab3 = Convert.ToInt32(Console.ReadLine());
-            int ab4 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(ab4 - ab3);
+            Console.WriteLine("Введите два числа, разницу которых вы хотите узнать");
+            int z = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(z - m);
             break;
         case 3:
-            Console.WriteLine("Напишите два числа");
-            int ab5 = Convert.ToInt32(Console.ReadLine());
-            int ab6 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(ab5 * ab6);
+            Console.WriteLine("Введите два числа, которые хотите умножить");
+            int t = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(t * y);
             break;
         case 4:
-            Console.WriteLine("Напишите два числа");
-            int ab7 = Convert.ToInt32(Console.ReadLine());
-            int ab8 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(ab7 / ab8);
+            Console.WriteLine("Введите два числа, которые хотите разделить");
+            int P = Convert.ToInt32(Console.ReadLine());
+            int u = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(P / u);
             break;
         case 5:
-            Console.WriteLine("Напишите два числа");
-            int ab9 = Convert.ToInt32(Console.ReadLine());
-            int ab10 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"{ab9}^{ab10} = {(long)Math.Pow(ab9, ab10):N0} (0x{(long)Math.Pow(ab9, ab10):X})");
+            Console.WriteLine("Напишите число, а затем степень в которую вы хотите возвести данное число");
+            int G = Convert.ToInt32(Console.ReadLine());
+            int D = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{(long)Math.Pow(G, D):N0}");
             break;
         case 6:
-            Console.WriteLine("Напишите число");
-            int ab11 = Convert.ToInt32(Math.Sqrt(5.25));
-            Console.WriteLine(ab11);
+            Console.WriteLine("Введите число, квадратный корень которого вы хотите найти");
+            int M = Convert.ToInt32(Math.Sqrt(5.25));
+            Console.WriteLine(M);
             break;
         case 7:
-            Console.WriteLine("Напишите число");
-            int ab12 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(ab12 % 1);
+            Console.WriteLine("Введите число 1% которого вы хотите узнать");
+            int C = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(C *0.01);
             break;
         case 8:
-            Console.WriteLine("Напишите число");
-            int ab13 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Не знаю как найти интеграл");
+            Console.WriteLine("Введите число факториал которого вы хотите найти");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int factorial = 1;  
+            for (int i = 2; i <= n; i++) 
+            {
+                factorial = factorial * i;
+            }
+            Console.WriteLine(factorial);
             break; 
         case 9:
             Console.WriteLine("Завершение программы.");
             break;
     }
-} while (oper != 9);
+} while (a != 9);
